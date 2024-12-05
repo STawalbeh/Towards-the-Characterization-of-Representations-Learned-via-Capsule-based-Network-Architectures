35f3fa7860a94585c13e213b3097c3fbb9e616ec
Saja Tawalbeh <82958017+STawalbeh@users.noreply.github.com>
2024-12-05 12:31:17 +0100

    Create capsuleLoss.py

---
+++ Capsule Networks/Residual Routing/loss/capsuleLoss.py
@@ -0,0 +1,18 @@
+import torch
+from mainProjects.Models.emRouting.loss.spreadLoss import SpreadLoss
+from torch.nn.modules.loss import _Loss
+
+class CapsuleLoss(_Loss):
+    def __init__(self, m_min=0.2, m_max=0.9, num_class=10):
+        super().__init__()
+        self.spread = SpreadLoss(m_min=m_min, m_max=m_max, num_class=num_class)
+        self.recon = torch.nn.MSELoss(reduction="sum")
+
+    def forward(self, x, target, images, reconstruction, r):
+        # spread loss
+        
+        spread_loss = self.spread(x, target, r)
+        reconstruction_loss = 5e-4 * self.recon(reconstruction, images.view(*reconstruction.shape))
+        loss = spread_loss + reconstruction_loss 
+        
+        return loss
