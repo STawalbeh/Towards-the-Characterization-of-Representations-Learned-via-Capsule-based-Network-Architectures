c0d7da91b339cf363758df2f9d882dc4ee07d260
Saja Tawalbeh <82958017+STawalbeh@users.noreply.github.com>
2025-01-02 19:14:42 +0100

    Update modeltraining.py

--- Model Training/modeltraining.py
+++ Model Training/modeltraining.py
@@ -1,11 +1,12 @@
 import time
 import torch.optim as optim
-from P1ModelInterpretation.Models.DynamicRouting.CapsuleLoss import CapsuleLoss
-from P1ModelInterpretation.Models.DynamicRouting.dynamicRouting import CapsuleNetwork
-from P1ModelInterpretation.ModelTraining.train import train
-from P1ModelInterpretation.ModelTraining.evaluate import evaluate
-from P1ModelInterpretation.Utilties.Visualization import trainingCurves
-
+from CapsuleNetworks.DynamicRouting.CapsuleLoss import CapsuleLoss
+from CapsuleNetworks.DynamicRouting.dynamicRouting import CapsuleNetwork
+from ModelTraining.train import train
+from ModelTraining.evaluate import evaluate
+from CapsuleNetworks.emRouting.network import *
+from CapsuleNetworks.ResidualRouting.network import *
+from CapsuleNetworks.emRouting.loss.capsuleLoss import CapsuleLoss
 
 def modelTraining(args, device, trainLoader, valLoader, testLoader):
     t = time.time()
@@ -17,13 +18,25 @@ def modelTraining(args, device, trainLoader, valLoader, testLoader):
     test_acc = []
 
     ''' Network Loading'''
-    criterion = CapsuleLoss()
-    CapsNet = CapsuleNetwork(args)
-    CapsNet = CapsNet.to(device)
-    optimizer = optim.Adam(CapsNet.parameters())
+    if args.mode == 'DR':
+        criterion = CapsuleLoss()
+        CapsNet = CapsuleNetwork(args)
+        CapsNet = CapsNet.to(device)
+        optimizer = optim.Adam(CapsNet.parameters())
+    elif args.mode == 'EM':
+        CapsNet = capsules(args).to(device)
+        criterion = CapsuleLoss(num_class=args.classesNum, m_min=0.2, m_max=0.9)
+        optimizer = optim.Adam(CapsNet.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        CapsNet = CapsNet.to(device)
+    elif args.mode == 'RR':
+        CapsNet = capsulesRR(args).to(device)
+        criterion = CapsuleLoss(num_class=args.classesNum, m_min=0.2, m_max=0.9)
+        optimizer = optim.Adam(CapsNet.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        #scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=1)
+        CapsNet = CapsNet.to(device)
 
     for epoch in range(args.nEpochs):
-        train_loss, train_accuracy = train(CapsNet, trainLoader, optimizer, criterion, device)
+        train_loss, train_accuracy = train(args, CapsNet, trainLoader, optimizer, criterion, device)
         valid_loss, valid_accuracy = evaluate(CapsNet, valLoader, criterion, device, args)
         test_loss, test_accuracy = evaluate(CapsNet, testLoader, criterion, device, args)
 
@@ -41,14 +54,3 @@ def modelTraining(args, device, trainLoader, valLoader, testLoader):
     print('train acc ', train_acc)
     print('valid acc ', valid_acc)
     print('test acc ', test_acc)
-
-    # Visualize the accuracy curves
-    saveImgPath= 'YOUR path'
-    label= 'Accuracy'
-    title= 'TrainingAccuracies'
-    trainingCurves(train_acc, valid_acc, test_acc, label, title, saveImgPath)
-    # Visualize the loss curves
-    label= 'Losses'
-    title= 'TrainingLosses'
-    trainingCurves(tLoss, vLoss, tsLoss, label, title, saveImgPath)
-
